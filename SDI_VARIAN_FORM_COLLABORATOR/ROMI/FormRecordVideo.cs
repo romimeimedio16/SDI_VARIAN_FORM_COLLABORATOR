8370aae4939e846c0284a0a1ee722f0637898443
Romi Meimedio <meimedioromi@gmail.com>
2025-07-28 14:09:01 +0700

    Add Romi

---
+++ SDI_VARIAN_FORM_COLLABORATOR/ROMI/FormRecordVideo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SDI_VARIAN_FORM_COLLABORATOR.ROMI
+{
+    public partial class FormRecordVideo: Form
+    {
+        public FormRecordVideo()
+        {
+            InitializeComponent();
+        }
+    }
+}
